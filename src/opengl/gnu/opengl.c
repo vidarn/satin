184d821c581904514bc79a1e23582a07aecd46ec
Vidar Nelson <vidar.nelson@gmail.com>
2019-02-28 12:02:04 +0100

    Button press on GNU

--- src/opengl/gnu/opengl.c
+++ src/opengl/gnu/opengl.c
@@ -1,13 +1,17 @@
 #include "opengl.h"
+#define GLX_MESA_swap_control 1
 #include <GL/glx.h>
 #define OPENGL_FUNC(type, name) type name;
 #include "opengl_funcs.h"
 #undef OPENGL_FUNC
+typedef int (* PFNGLXSWAPINTERVALMESAPROC) (unsigned int interval);
+PFNGLXSWAPINTERVALMESAPROC glXSwapIntervalMESA;
 
 void opengl_load(void)
 {
 	#define OPENGL_FUNC(type, name) name = (void*)glXGetProcAddress(#name);
 	#include "opengl_funcs.h"
 	#undef OPENGL_FUNC
+	glXSwapIntervalMESA = (void*)glXGetProcAddress("glXSwapIntervalMESA");
 
 }
