21554efe50b606e9a90fb04f9b16ad1bdc18545c
Vidar Nelson <vidar.nelson@gmail.com>
2019-02-28 12:15:53 +0100

    Fixed click on windows

--- src/os/win32/os.c
+++ src/os/win32/os.c
@@ -436,9 +436,12 @@ void launch_game(const char *window_title, int _framebuffer_w, int _framebuffer_
 			{
 				input_state.mouse_state = MOUSE_CLICKED;
 				input_state.mouse_down = 1;
-				POINTS tmp = MAKEPOINTS(msg.lParam);
-				drag_start_x = tmp.x;
-				drag_start_y = tmp.y;
+				//POINTS tmp = MAKEPOINTS(msg.lParam);
+				POINT p;
+				GetCursorPos(&p);
+				ScreenToClient(hWnd, &p);
+				drag_start_x = p.x;
+				drag_start_y = p.y;
 				break;
 			}
 			case WM_LBUTTONUP:
@@ -536,8 +539,8 @@ void launch_game(const char *window_title, int _framebuffer_w, int _framebuffer_
 				float dx = fabsf(input_state.drag_start_x - input_state.mouse_x);
 				float dy = fabsf(input_state.drag_start_y - input_state.mouse_y);
 				//printf("%f %f, %f %f\n",input_state.mouse_x,input_state.mouse_y,input_state.drag_start_x,input_state.drag_start_y);
-				float drag_dx = (float)GetSystemMetrics(SM_CXDRAG) / (float)(r.right - r.left);
-				float drag_dy = (float)GetSystemMetrics(SM_CYDRAG) / (float)(r.top - r.bottom);
+				float drag_dx = fabsf((float)GetSystemMetrics(SM_CXDRAG) / (float)(r.right - r.left));
+				float drag_dy = fabsf((float)GetSystemMetrics(SM_CYDRAG) / (float)(r.top - r.bottom));
 				if (dx>drag_dx || dy>drag_dy) {
 					input_state.mouse_state = MOUSE_DRAG;
 				}
