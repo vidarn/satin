20fe91ad1f039f63f5e9783a63ef529bc6f0d234
Peter McEvoy <peter.michael.mcevoy@gmail.com>
2019-11-04 11:20:52 +0100

    add create_sprite_atlas to .h file

--- src/engine.h
+++ src/engine.h
@@ -132,6 +132,8 @@ int load_mesh_from_memory(int num_verts, struct Vec3 *pos_data,
 void update_mesh_from_memory(int mesh_index, int num_verts, struct Vec3 *pos_data,
 	struct Vec3 *normal_data, struct Vec2 *uv_data, int num_tris,
 	int *tri_data, int shader, struct GameData *data);
+void create_sprite_atlas(struct GameData* data)
+;
 
 struct CustomMeshDataSpec
 {
